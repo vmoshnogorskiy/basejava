f75a2059a5f4ae9e2f6c4600dc6cf9c61a2f6f4d
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2022-07-01 12:41:12 +0300

    BaseJava: Lesson_5, copy classes from Lesson_4

--- src/com/basejava/lesson_4/exception/NotExistStorageException.java
+++
@@ -1,7 +0,0 @@
-package com.basejava.lesson_4.exception;
-
-public class NotExistStorageException extends StorageException {
-    public NotExistStorageException(String uuid) {
-        super("ERROR: Резюме с указанным uuid не существует в хранилище", uuid);
-    }
-}
