27a47462b0c944124d252ebaeaf4c49ea004b9e7
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2022-07-06 12:48:42 +0300

    BaseJava. Lesson_5, add MapStorage

---
+++ src/com/basejava/storage/MapStorage.java
@@ -0,0 +1,58 @@
+package com.basejava.storage;
+
+import com.basejava.model.Resume;
+
+import java.util.HashMap;
+
+public class MapStorage extends AbstractStorage {
+
+    protected HashMap<String, Resume> storage = new HashMap<String, Resume>();
+
+    @Override
+    protected Object findKey(String uuid) {
+        if (storage.containsKey(uuid)) {
+            return uuid;
+        }
+        return null;
+    }
+
+    @Override
+    protected void doUpdate(Resume resume, Object key) {
+        storage.put((String) key, resume);
+    }
+
+    @Override
+    protected void doSave(Resume r) {
+        storage.put(r.getUuid(), r);
+    }
+
+    @Override
+    protected Resume doGet(Object key) {
+        return storage.get((String) key);
+    }
+
+    @Override
+    protected void doDelete(Object key) {
+        storage.remove((String) key);
+    }
+
+    @Override
+    protected boolean isExist(Object key) {
+        return !(key == null);
+    }
+
+    @Override
+    public void clear() {
+        storage.clear();
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return storage.values().toArray(new Resume[storage.size()]);
+    }
+
+    @Override
+    public int size() {
+        return storage.size();
+    }
+}
