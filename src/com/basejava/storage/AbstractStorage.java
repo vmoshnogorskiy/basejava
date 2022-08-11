6ba63b7eb40411ee0068270528281e646547249b
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2022-08-11 10:42:36 +0300

    BaseJava. Lesson_7, Add ENUM SectionType, Add LOG

--- src/com/basejava/storage/AbstractStorage.java
+++ src/com/basejava/storage/AbstractStorage.java
@@ -6,32 +6,38 @@ import com.basejava.model.Resume;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
     private static final Comparator<Resume> COMPARATOR_RESUME = Comparator.comparing(Resume::getFullName)
             .thenComparing(Resume::getUuid);
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
 
     @Override
     public void update(Resume r) {
+        LOG.info("Update " + r);
         SK key = findExistingKey(r.getUuid());
         doUpdate(r, key);
     }
 
     @Override
     public void save(Resume r) {
+        LOG.info("Save " + r);
         SK key = findNotExistingKey(r.getUuid());
         doSave(r, key);
     }
 
     @Override
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK key = findExistingKey(uuid);
         return doGet(key);
     }
 
     @Override
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK key = findExistingKey(uuid);
         doDelete(key);
     }
@@ -39,6 +45,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected SK findExistingKey(String uuid) {
         SK key = findKey(uuid);
         if (!isExist(key)) {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new NotExistStorageException(uuid);
         }
         return key;
@@ -47,6 +54,7 @@ public abstract class AbstractStorage<SK> implements Storage {
     protected SK findNotExistingKey(String uuid) {
         SK key = findKey(uuid);
         if (isExist(key)) {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new ExistStorageException(uuid);
         }
         return key;
@@ -54,6 +62,7 @@ public abstract class AbstractStorage<SK> implements Storage {
 
     @Override
     public List<Resume> getAllSorted() {
+        LOG.info("getAllSorted");
         List<Resume> list = doGetAll();
         list.sort(COMPARATOR_RESUME);
         return list;
