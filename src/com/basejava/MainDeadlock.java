a2cfd64e56ba9ba14da68dcc81020fd431dfbcfc
Vladimir Moshnogorskiy <il75ich@yandex.ru>
2022-09-28 16:05:38 +0300

    BaseJava. Lesson_11: Refactor MainDeadlock

--- src/com/basejava/MainDeadlock.java
+++ src/com/basejava/MainDeadlock.java
@@ -3,36 +3,29 @@ package com.basejava;
 public class MainDeadlock {
 
     public static void main(String[] args) {
-        Object a = new Object();
-        Object b = new Object();
+        final Object LOCK_1 = new Object();
+        final Object LOCK_2 = new Object();
 
         new Thread(() -> {
-            synchronized (a) {
-                System.out.println(Thread.currentThread().getName() + " is taken Object 'a' monitor");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    System.out.println(Thread.currentThread().getName() + " sleep interrupted");
-                }
-                System.out.println(Thread.currentThread().getName() + " trying to take 'b' monitor..");
-                synchronized (b) {
-                    System.out.println(Thread.currentThread().getName() + " is taken Object 'b' monitor");
-                }
-            }
+            MainDeadlock.localRun(LOCK_1, LOCK_2);
         }).start();
         new Thread(() -> {
-            synchronized (b) {
-                System.out.println(Thread.currentThread().getName() + " is taken Object 'b' monitor");
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    System.out.println(Thread.currentThread().getName() + " sleep interrupted");
-                }
-                System.out.println(Thread.currentThread().getName() + " trying to take 'a' monitor..");
-                synchronized (a) {
-                    System.out.println(Thread.currentThread().getName() + " is taken Object 'a' monitor");
-                }
-            }
+            MainDeadlock.localRun(LOCK_2, LOCK_1);
         }).start();
+    }
+
+    private static void localRun(Object lockA, Object lockB) {
+        synchronized (lockA) {
+            System.out.println(Thread.currentThread().getName() + " is taken Object " + lockA.toString() + " monitor");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println(Thread.currentThread().getName() + " sleep interrupted");
+            }
+            System.out.println(Thread.currentThread().getName() + " trying to take " + lockB.toString() + " monitor..");
+            synchronized (lockB) {
+                System.out.println(Thread.currentThread().getName() + " is taken Object " + lockB.toString() + " monitor");
+            }
+        }
     }
 }
